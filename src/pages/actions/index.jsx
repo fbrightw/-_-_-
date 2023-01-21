5b3bb8959fc10316b4294edd21a4296e456c5079
fbrightw <gasanova_1998@bk.ru>
2023-01-21 22:34:22 +0300

    *  page with statuses is working with redirect

--- src/pages/actions/index.jsx
+++ src/pages/actions/index.jsx
@@ -2,32 +2,49 @@ import React from 'react';
 import TitleForm from "../../utils/TitleForm";
 import {Card, ListGroup} from "react-bootstrap";
 import {useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ListItemForm from "../../utils/ListItemForm";
 
-export default function (props) {
+export default function() {
 
-  const removedGifts = useSelector(state => state.giftArray.removedGifts)
-  console.log("removed", removedGifts)
+  const removedGifts = useSelector(state => state.giftArray.removedGifts);
+  const givenGifts = useSelector(state => state.giftArray.givenGifts);
+  let navigate = useNavigate();
+
+  function onClick() {
+    navigate('/home')
+  }
 
   return (
-      <div>
+      <div className="centering-container">
         <TitleForm title="История действий:"/>
-        <Card className="card">
+        <Card>
           <ListGroup variant="flush">
-            {
-              <div>
-                <ListGroup.Item>
-                  <div className="card-header">
-                    {gift.species}
-                    <button
-                        className="delete-text"
-                        value={gift.id}
-                        onClick={(e) => props.onDelete(e.target.value)}
-                    >
-                      Удалить
-                    </button>
-                  </div>
-                </ListGroup.Item>
-              </div>
+            {removedGifts.length > 0 ?
+              removedGifts.map(gift =>
+                <ListItemForm
+                  key={gift.id}
+                  status="Удален"
+                  gift={gift}
+                  buttonText="Вернуться"
+                  onClick={() => onClick}
+                />
+                )
+                :
+                null
+            }
+            {givenGifts.length > 0 ?
+                givenGifts.map(gift =>
+                    <ListItemForm
+                        key={gift.id}
+                        status="Подарен"
+                        gift={gift}
+                        buttonText="Вернуться"
+                        onClick={onClick}
+                    />
+                )
+                :
+                null
             }
           </ListGroup>
         </Card>
