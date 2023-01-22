8a97fc49368c1ebe6cd80e19e29699550e9d7502
fbrightw <gasanova_1998@bk.ru>
2023-01-22 20:29:09 +0300

    *  filter forms inline

--- src/components/pages/home/Filter.js
+++ src/components/pages/home/Filter.js
@@ -39,71 +39,75 @@ function Filter(props) {
       <div className="centering-container">
         <TitleForm title="Подарить подарок"/>
         <div className="filter">
-          <Form.Group>
-            <Form.Select
-                aria-label="Default select example"
-                name="species"
-                isInvalid={ !!props.errors.species }
-                onChange={(e) => onDropdownClick(e.target.value)}
-            >
-              <option>Вид животного</option>
-              {animalSpecies.map(el => (
-                  <option
-                      value={el.name}
-                      key={el.id}
-                  >
-                    {el.name}
-                  </option>
-              ))}
-            </Form.Select>
-            <Form.Control.Feedback type='invalid'>
-              { props.errors.species }
-            </Form.Control.Feedback>
-            <div  key={`inline-radio`} className="mb-3">
-              <Form.Check
-                  label="М"
-                  name="group1"
-                  type={'radio'}
-                  id={'inline-radio-1'}
-                  inline
-                  value="М"
-                  onClick={(e) => onGenderClick(e.target.value)}
-                  isInvalid={ !!props.errors.gender }
-              />
-              <Form.Check
-                  label="Ж"
-                  name="group1"
-                  type={'radio'}
-                  id={'inline-radio-2'}
-                  inline
-                  value="Ж"
-                  onClick={(e) => onGenderClick(e.target.value)}
-                  isInvalid={ !!props.errors.gender }
+            <Form.Group>
+              <Form.Select
+                  aria-label="Default select example"
+                  name="species"
+                  isInvalid={ !!props.errors.species }
+                  onChange={(e) => onDropdownClick(e.target.value)}
+              >
+                <option>Вид животного</option>
+                {animalSpecies.map(el => (
+                    <option
+                        value={el.name}
+                        key={el.id}
+                    >
+                      {el.name}
+                    </option>
+                ))}
+              </Form.Select>
+              <Form.Control.Feedback type='invalid'>
+                { props.errors.species }
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <div  key={`inline-radio`} className="mb-3">
+                <Form.Check
+                    label="М"
+                    name="group1"
+                    type={'radio'}
+                    id={'inline-radio-1'}
+                    inline
+                    value="М"
+                    onClick={(e) => onGenderClick(e.target.value)}
+                    isInvalid={ !!props.errors.gender }
+                />
+                <Form.Check
+                    label="Ж"
+                    name="group1"
+                    type={'radio'}
+                    id={'inline-radio-2'}
+                    inline
+                    value="Ж"
+                    onClick={(e) => onGenderClick(e.target.value)}
+                    isInvalid={ !!props.errors.gender }
+                />
+                <Form.Control.Feedback type='invalid'>
+                  { props.errors.gender }
+                </Form.Control.Feedback>
+              </div>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                  size="df"
+                  type="text"
+                  value={obj.name}
+                  onChange={(e) => onInput(e)}
+                  placeholder="Укажите возраст"
+                  isInvalid={ !!props.errors.age }
               />
               <Form.Control.Feedback type='invalid'>
-                { props.errors.gender }
+                { props.errors.age }
               </Form.Control.Feedback>
-            </div>
-            <Form.Control
-                size="df"
-                type="text"
-                value={obj.name}
-                onChange={(e) => onInput(e)}
-                placeholder="Укажите возраст"
-                isInvalid={ !!props.errors.age }
-            />
-            <Form.Control.Feedback type='invalid'>
-              { props.errors.age }
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button variant="dark" onClick={() => {
-            props.onAdd(obj, count);
-            setCount(count + 1);
-          }}>
+            </Form.Group>
+            <Button variant="dark" onClick={() => {
+              props.onAdd(obj, count);
+              setCount(count + 1);
+            }}>
               Добавить
-          </Button>
+            </Button>
+          </div>
         </div>
-      </div>
   );
 }
 
